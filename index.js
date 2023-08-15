9236706e3c18521175f4bae128326757bd2130a7
Fahad <fahadsaadalzahrani8@gmail.com>
2023-08-15 21:13:29 +0300

    Finshed with comments

---
+++ index.js
@@ -0,0 +1,13 @@
+let name = window.prompt("Your Name")
+let age = window.prompt("Your Age")
+if (age >= 18){
+    alert("Wellcome " + name)
+}else{
+    alert("Am Sorry " + name + " But Your To Young To Enter")
+}
+let name_1 = window.prompt("Your Name")
+if (name_1 == "Ahmed"){
+    alert("The Two Names Are The Same")
+}else{
+    alert("The Two Names Are Not The Same")
+}
